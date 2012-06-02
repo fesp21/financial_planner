02e3045fb074e3d14fbeeac43755586b2290d57c
Austin Van Blanton <imaus10@gmail.com>
2012-06-02 10:42:05 -0400

    Make money_field initially blank, not 0.00

--- app/helpers/my_custom_form_builder.rb
+++ app/helpers/my_custom_form_builder.rb
@@ -1,7 +1,13 @@
 class MyCustomFormBuilder < ActionView::Helpers::FormBuilder
 	def money_field(method, options = {})
 		value = @object.send(method)
-		formatted_value = value.original_value || value.format(symbol: false)
+		if value.original_value
+			formatted_value = value.original_value
+		elsif value.cents == 0
+			formatted_value = ""
+		else
+			formatted_value = value.format(symbol: false)
+		end
 		text_field method, options.merge(:value => (formatted_value))
 	end
 end
