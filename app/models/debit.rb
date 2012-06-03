1a5ad82bae0e34480f3f0e1fc52a3e9e42467a27
Austin Van Blanton <imaus10@gmail.com>
2012-06-03 14:28:11 -0400

    Annotate debit model

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: debits
+#
+#  id               :integer         not null, primary key
+#  cost             :integer         not null
+#  description      :text
+#  category_id      :integer
+#  transaction_date :date
+#  user_id          :integer         not null
+#  created_at       :datetime        not null
+#  updated_at       :datetime        not null
+#
+
 include MoneyModule
 
 class Debit < ActiveRecord::Base
