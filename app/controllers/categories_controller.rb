90efa8e0eae495f3f8478bf7702740d2673c396c
Austin Van Blanton <imaus10@gmail.com>
2012-06-05 18:40:55 -0400

    Use redirect_to instead of render

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,17 @@
 class CategoriesController < ApplicationController
-  before_filter :signed_in_user
+	before_filter :signed_in_user
 
-  def new
-  	@category = Category.new
-  end
+	def new
+		@category = Category.new
+	end
 
-  def create
-  	@category = Category.new(params[:category])
-  	if @category.save
-  		flash.now[:success] = "Category successfully added."
-  		@category = Category.new
-  	end
-  	render 'new'
-  end
+	def create
+		@category = Category.new(params[:category])
+		if @category.save
+			flash[:success] = "Category successfully added."
+			redirect_to new_category_path
+		else
+			render 'new'
+		end
+	end
 end
--- app/controllers/debits_controller.rb
+++ app/controllers/debits_controller.rb
@@ -1,16 +1,17 @@
 class DebitsController < ApplicationController
 	before_filter :signed_in_user
 
-  def new
-  	@debit = Debit.new(transaction_date: Time.now.strftime("%m/%d/%Y"))
-  end
+	def new
+		@debit = Debit.new(transaction_date: Time.now.strftime("%m/%d/%Y"))
+	end
 
-  def create
-  	@debit = current_user.debits.build(params[:debit])
-  	if @debit.save
-  		flash.now[:success] = "Debit deducted. *flush*"
-  		@debit = Debit.new(transaction_date: Time.now.strftime("%m/%d/%Y"))
-  	end
-  	render 'new'
-  end
+	def create
+		@debit = current_user.debits.build(params[:debit])
+		if @debit.save
+			flash[:success] = "Debit deducted. *flush*"
+			redirect_to new_debit_path
+		else
+			render 'new'
+		end
+	end
 end
