c0632a7dc8a6da342b06b46a870ea99131b16d62
Austin Van Blanton <imaus10@gmail.com>
2012-06-05 21:19:17 -0400

    Add a categories index with delete links

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,10 +8,20 @@ class CategoriesController < ApplicationController
 	def create
 		@category = Category.new(params[:category])
 		if @category.save
-			flash[:success] = "Category successfully added."
+			flash[:success] = "Category added."
 			redirect_to new_category_path
 		else
 			render 'new'
 		end
+	end
+
+	def destroy
+		@category = Category.find(params[:id]).destroy
+		flash[:success] = "Category deleted, you monster."
+		redirect_to categories_path
+	end
+
+	def index
+		@categories = Category.paginate(:page => params[:page])
 	end
 end
