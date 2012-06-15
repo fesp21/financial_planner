826eb00e4ab8e2edc938fc0d281b1af9694b3eac
Austin Van Blanton <imaus10@gmail.com>
2012-06-14 23:09:21 -0400

    Move category new into index following the transactions paradigm

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,25 @@
 class CategoriesController < ApplicationController
 	before_filter :signed_in_user
 
-	def new
-		@category = Category.new
-	end
-
 	def create
 		@category = Category.new(params[:category])
 		if @category.save
-			flash[:success] = "Category added."
-			redirect_to new_category_path
+			flash[:success] = "Category added. Meow!"
+			redirect_to categories_path
 		else
-			render 'new'
+			@categories = Category.paginate(:page => params[:page])
+			render 'index'
 		end
 	end
 
 	def destroy
 		@category = Category.find(params[:id]).destroy
-		flash[:success] = "Category deleted, you monster."
+		flash[:success] = "Category exterminated, you monster."
 		redirect_to categories_path
 	end
 
 	def index
 		@categories = Category.paginate(:page => params[:page])
+		@category = Category.new
 	end
 end
