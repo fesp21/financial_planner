29524972b6c4ae1e51f08b0bccf41101860963bc
Austin Van Blanton <imaus10@gmail.com>
2012-08-01 20:58:31 -0400

    Make home page show current month

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
 	def home
-		@total = Money.us_dollar(Credit.sum(:amount) - Debit.sum(:amount))
+		if signed_in?
+			monthCred = Credit.where("extract(month from transaction_date) = ?", Time.now.month).sum(:amount)
+			monthDeb = Debit.where("extract(month from transaction_date) = ?", Time.now.month).sum(:amount)
+			@total = Money.us_dollar(monthCred - monthDeb)
+		end
 	end
 end
