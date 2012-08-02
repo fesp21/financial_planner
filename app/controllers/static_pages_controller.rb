bfaa7c91608b2c395eb4946a7e813cb3cc5df2f5
Austin Van Blanton <imaus10@gmail.com>
2012-08-01 21:06:01 -0400

    Deal with no transactions on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,11 @@ class StaticPagesController < ApplicationController
 		if signed_in?
 			monthCred = Credit.where("extract(month from transaction_date) = ?", Time.now.month).sum(:amount)
 			monthDeb = Debit.where("extract(month from transaction_date) = ?", Time.now.month).sum(:amount)
-			@total = Money.us_dollar(monthCred - monthDeb)
+			@empty = true
+			if monthCred != 0 and monthDeb != 0
+				@total = Money.us_dollar(monthCred - monthDeb)
+				@empty = false
+			end
 		end
 	end
 end
